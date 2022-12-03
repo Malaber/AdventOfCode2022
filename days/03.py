70de3d81140a6c4ebb43788def7e9d3aa8a6b439
Malaber <git@schaedler.rocks>
2022-12-04 00:32:14 +0100

    Add day 03 part 2

--- days/03.py
+++ days/03.py
@@ -7,7 +7,18 @@ from helpers.input import Input
 
 lines = Input.get_lines(os.path.basename(__file__))
 
+
+def get_priority_for_char(c: str):
+    prio = ord(c)
+    if prio < 91:
+        prio -= 38
+    else:
+        prio -= 96
+    return prio
+
+
 priority_sum = 0
+priority_sum_2 = 0
 
 for line in lines:
     line = list(line.strip())
@@ -19,12 +30,24 @@ for line in lines:
         if char in right:
             duplicate = char
             break
-    priority = ord(duplicate)
-    if priority < 91:
-        priority -= 38
-    else:
-        priority -= 96
+    priority = get_priority_for_char(duplicate)
     priority_sum += priority
+
+lines = [list(s.strip()) for s in lines]
+lines = iter(lines)
+for line in lines:
+    next_line = next(lines)
+    more_next_line = next(lines)
+    badge = None
+    for char in line:
+        if char in next_line and char in more_next_line:
+            badge = char
+            break
+    priority = get_priority_for_char(badge)
+    priority_sum_2 += priority
 
 print("Part 1")
 print(priority_sum)
+print()
+print("Part 2")
+print(priority_sum_2)
