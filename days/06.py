2046b822762851e9308b8698d0b40bf36aceb1ac
Malaber <32635600+Malaber@users.noreply.github.com>
2022-12-06 20:35:53 +0000

    Add day 6 with test input

---
+++ days/06.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))  # I am not proud of this
+
+from helpers.input import Input
+
+lines = Input.get_lines(os.path.basename(__file__))
+
+chars = list(lines[0].strip())
+window = []
+
+for index, char in enumerate(chars):
+    window.append(char)
+    if len(window) > 4:
+        window.pop(0)
+
+    if index < 3:
+        continue
+
+    if len(window) == len(list(set(window))):
+        print(index + 1)
+        break
