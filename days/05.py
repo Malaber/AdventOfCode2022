71d70d7501e7ab60a9bef2232f768be5f7731280
Malaber <git@schaedler.rocks>
2022-12-07 11:43:19 +0100

    Improve readability of day 5

--- days/05.py
+++ days/05.py
@@ -5,27 +5,49 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))  # I am not proud
 
 from helpers.input import Input
 
-lines = Input.get_lines(os.path.basename(__file__))
+lines_input = Input.get_lines(os.path.basename(__file__))
 
-lines.remove("\n")
+lines = []
+# line preprocessing
+for line in lines_input:
+    if not line == "\n":
+        # remove newlines at end of the last words
+        words = [word.strip() for word in line.split(" ")]
+        lines.append(words)
 
 
 def combine_4_spaces_into_single_column(words):
+    """
+    Combines 4 empty strings in list into one.
+
+    If given the following input:
+    L1'    [Q] [F] [W] [S] [V] [N] [F] [N]'
+    L2' 1   2   3   4   5   6   7   8   9 '
+
+    this results in the following words for L1:
+    ["", "", "", "", "[Q]", "[F]", "[W]", "[S]", "[V]", "[N]", "[F]", "[N]"]
+    Therefore we have to combine the first 4 empty strings into a single one
+
+    :param words: List of words resulting from the current line
+    :return: List of words with every 4 spaces combined into one
+    """
     columns = []
     words = iter(words)
     for word in words:
-        if word == "" and next(words) == "" and next(words) == "" and next(words).strip() == "":
+        if word == "" and next(words) == "" and next(words) == "" and next(words) == "":
             columns.append("")
         else:
-            columns.append(word.strip())
+            columns.append(word)
 
     return columns
 
 
-built_stacks = False
+first_line_words = lines[0]
+stacks_needed = len(combine_4_spaces_into_single_column(first_line_words))
+# prepare as many empty stacks as we need
 stacks = {}
 stacks_2 = {}
-for i in range(1, 10):
+for i in range(1, stacks_needed + 1):
     stacks[i] = []
     stacks_2[i] = []
 
@@ -33,7 +55,7 @@ for i in range(1, 10):
 def handle_move(words, stacks):
     amount = int(words[1])
     fromm = int(words[3])  # naming a variable "from" causes trouble lol
-    to = int(words[5].strip())
+    to = int(words[5])
 
     for _i in range(0, amount):
         stack_to = stacks[to]
@@ -47,7 +69,7 @@ def handle_move(words, stacks):
 def handle_move_2(words, stacks):
     amount = int(words[1])
     fromm = int(words[3])  # naming a variable "from" causes trouble lol
-    to = int(words[5].strip())
+    to = int(words[5])
 
     buffer = []
     for _i in range(0, amount):
@@ -61,9 +83,8 @@ def handle_move_2(words, stacks):
     return stacks_2
 
 
-for line in lines:
-    words = line.split(" ")
-    print()
+# iterate over each line
+for words in lines:
     match words[0]:
         case "1":
             continue
@@ -84,5 +105,6 @@ answer = "".join([stack.pop() for stack in stacks.values()])
 answer_2 = "".join([stack.pop() for stack in stacks_2.values()])
 print("Part 1:")
 print(answer)
+print()
 print("Part 2:")
 print(answer_2)
